15c5cc224d794f5099fcc274a6c27c117422d950
kimdaeyeub <kdy3915@naver.com>
2024-11-17 02:12:01 +0900

    Setup Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <div className="background-light850_dark100 text-5xl">
-      Welcome to the world of Next.js
-    </div>
+    <>
+      <h1 className="h1-bold text-3xl">Welcome to the world of Next.js</h1>
+      <h1 className="h1-bold font-inter text-3xl">
+        Welcome to the world of Next.js
+      </h1>
+      <h1 className="h1-bold font-space-grotesk text-3xl">
+        Welcome to the world of Next.js
+      </h1>
+    </>
   );
 }
